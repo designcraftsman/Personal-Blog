c2cb4d2c9b7546b1667ef79ada425ca979e394a3
Oussama Fayz <fzoussfr@gmail.com>
2023-11-08 16:34:36 +0100

    Created the navbar

---
+++ navbar.php
@@ -0,0 +1,15 @@
+ <div class="navbar">
+    <h1 class="navbar__logo">Ali<span class="navbar__logo__style">ce</span></h1>
+    <nav >
+        <ul class="navbar__nav">
+            <li class="navbar__nav__link"><a href="#">Home</a></li>
+            <li class="navbar__nav__link"><a href="#">Blog</a></li>
+            <li class="navbar__nav__link"><a href="#">Categories</a></li>
+            <li class="navbar__nav__link"><a href="#">Contact</a></li>
+            <li class="navbar__nav__link"><a href="#">About</a></li>
+        </ul>
+    </nav>
+    <div class="navbar__search">
+    <i class="fa-solid fa-magnifying-glass fa-xl"></i>
+    </div>
+ </div>
