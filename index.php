a214e8101927e7bed3599d31eb3c8f1d18adbe8d
Oussama Fayz <fzoussfr@gmail.com>
2023-11-08 19:03:50 +0100

    created new section but still got issues with the grid

--- index.php
+++ index.php
@@ -8,6 +8,23 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>  
 <body>
-    <?php include('navbar.php'); ?>
+<?php include('navbar.php'); ?>
+<section id="home">
+    <div class="Posts">
+        <div class="Posts__post1">
+            <img src="https://images.pexels.com/photos/18596173/pexels-photo-18596173/free-photo-of-mode-femme-jardin-etre-assis.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"  alt="something">
+            <h2>Title</h2>
+        </div>
+        <div class="Posts__post2">
+            <img src="https://images.pexels.com/photos/15836393/pexels-photo-15836393/free-photo-of-tasse-feuilles-a-feuilles-persistantes-decore.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="something">
+            <h2>Title</h2>
+        </div>
+        <div class="Posts__post3">
+            <img src="https://images.pexels.com/photos/15836393/pexels-photo-15836393/free-photo-of-tasse-feuilles-a-feuilles-persistantes-decore.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="something">
+            <h2>Title</h2>
+        </div>
+    </div>
+
+</section>
 </body>
 </html>
