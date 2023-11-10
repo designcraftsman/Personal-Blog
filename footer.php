a32bdbdf9c1d5ae0f9a210a1e4a179fddd9b34e3
Oussama Fayz <fzoussfr@gmail.com>
2023-11-10 11:29:50 +0100

    copyright and license in footer

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 <footer id="footer">
     <div class="footer">
-        <h2 class="footer__brand"><span class="footer__brand__style">ALI</span>CE</h2>
         <div class="footer__socials">
             <h2 class="footer__socials__title">Socials</h2>
             <ul >
@@ -10,7 +9,11 @@
                 <li class="footer__socials__link"><a href="#"><i class="fa-brands fa-twitter fa-2xl" style="color: #ffffff;"></i></a></li>
             </ul>
         </div>
-        <div class="footer__contact">
+        <div class="footer__about"> 
+            <p >© 2023, Stories - Personal Blog HTML Template</p>
+            <p>Design by AliThemes | All rights reserved</p>
+        </div>
+            <div class="footer__contact">
             <h2 class="footer__contact__h2">Contact Us</h2>
         </div>
     </div>
