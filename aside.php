999d741186fdec7b2b38bc2a540e2bc96740e432
Oussama Fayz <fzoussfr@gmail.com>
2023-11-16 12:19:43 +0100

    added the aside setion and content to the blog page

---
+++ aside.php
@@ -0,0 +1,57 @@
+<aside class="asideSection">
+        <h2 class="asideSection__title">Recent Posts</h2>
+        <div class="asideSection__recentPosts">
+            <div class="asideSection__recentPosts__post">
+                <div class="asideSection__recentPosts__post__img">
+                    <img src="https://images.pexels.com/photos/613508/pexels-photo-613508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                </div>
+                <div class="asideSection__recentPosts__post__content">
+                    <a href="#">Lifestyle</a>
+                    <h3>9 Most Awesome Blue Lake With Snow Mountain</h3>
+                    <p>july 9 2023 </p>
+                 </div>   
+            </div>
+            <div class="asideSection__recentPosts__post">
+                <div class="asideSection__recentPosts__post__img">
+                    <img src="https://images.pexels.com/photos/613508/pexels-photo-613508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                </div>
+                <div class="asideSection__recentPosts__post__content">
+                    <a href="#">Lifestyle</a>
+                    <h3>9 Most Awesome Blue Lake With Snow Mountain</h3>
+                    <p>july 9 2023 </p>
+                 </div>   
+            </div>
+            <div class="asideSection__recentPosts__post">
+                <div class="asideSection__recentPosts__post__img">
+                    <img src="https://images.pexels.com/photos/613508/pexels-photo-613508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                </div>
+                <div class="asideSection__recentPosts__post__content">
+                    <a href="#">Lifestyle</a>
+                    <h3>9 Most Awesome Blue Lake With Snow Mountain</h3>
+                    <p>july 9 2023 </p>
+                 </div>   
+            </div>
+            <div class="asideSection__recentPosts__post">
+                <div class="asideSection__recentPosts__post__img">
+                    <img src="https://images.pexels.com/photos/1268099/pexels-photo-1268099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                </div>
+                <div class="asideSection__recentPosts__post__content">
+                    <a href="#">Lifestyle</a>
+                    <h3>9 Most Awesome Blue Lake With Snow Mountain</h3>
+                    <p>july 9 2023 </p>
+                 </div>   
+            </div>
+        </div>
+        <h2 class="asideSection__title">Browse Categories</h2>
+        <div class="asideSection__categories">
+           
+            <div class="asideSection__categories__categorie">Sport <span class="asideSection__categories__categorie__number">(15)</span></div>
+            
+            <div class="asideSection__categories__categorie">Lifestyle <span class="asideSection__categories__categorie__number">(07)</span></div>
+            
+            <div class="asideSection__categories__categorie">Health <span class="asideSection__categories__categorie__number">(25)</span></div>
+           
+            <div class="asideSection__categories__categorie">Science <span class="asideSection__categories__categorie__number">(08)</span></div>
+           
+        </div>
+    </aside>
--- blog.php
+++ blog.php
@@ -8,7 +8,37 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>  
 <body>
-  <?php include('navbar.php');?>
-    HEllo  
+  <?php include('navbar.php');
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root');
+    }
+    catch (Exception $e)
+    {
+        die('Erreur : ' . $e->getMessage());
+    }
+    $sqlQuery = 'SELECT postTitle, postImg ,categorie, postContent FROM posts';
+    $postsStatement = $db->prepare($sqlQuery);
+    $postsStatement->execute();
+    $posts = $postsStatement->fetchAll();
+?>
+   <div class="postsContainer">
+        <main class="postsContainer__posts">
+        <?php  foreach($posts as $post){ ?>
+          <div class="postsContainer__posts__post">
+             <div class="postsContainer__posts__post__img">
+                <img src="<?php echo($post['postImg']); ?>" alt="">
+              </div>
+              <div class="postsContainer__posts__post__content">
+                <a href="#"><?php echo($post['categorie']); ?></a>
+                <h3><?php echo($post['postTitle']); ?></h3>
+                <p><?php echo($post['postContent']); ?></p>
+              </div>
+           </div>
+         <?php } ?>
+        </main>
+        <?php include('aside.php'); ?>
+   </div>
+   <?php include('footer.php'); ?>
 </body>
 </html>
