17b97776e5489da7be9d48c59d814123da3040cc
Oussama Fayz <fzoussfr@gmail.com>
2023-11-18 17:10:22 +0100

    back to work

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
 <?php include('navbar.php'); ?>
     <div class="contactContainer">
         <div class="contactContainer__img">
-            <img src="https://images.pexels.com/photos/7094/wood-coffee-laptop-hero-7094.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+            <img src="https://images.pexels.com/photos/851213/pexels-photo-851213.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
         </div>
         <div class="contactContainer__container">
             <h1 >Contact Us</h1>
@@ -18,7 +18,7 @@
                  <h2>Contact</h2>
                  <p>fayz@blog.com</p>
                   <ul class="contactContainer__container__contact__info__socials">
-                        <li ><a href="#"><i class="fa-brands fa-facebook fa" ></i></a></li>
+                     <li ><a href="#"><i class="fa-brands fa-facebook fa" ></i></a></li>
                      <li ><a href="#"><i class="fa-brands fa-instagram fa"></i></a></li>
                      <li ><a href="#"><i class="fa-brands fa-linkedin fa" ></i></a></li>
                       <li ><a href="#"><i class="fa-brands fa-twitter fa" ></i></a></li>
