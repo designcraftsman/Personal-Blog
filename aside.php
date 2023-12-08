06bdaf30c34f4cf694f8d9397823c7365c6f1ecb
Oussama Fayz <fzoussfr@gmail.com>
2023-12-08 18:56:32 +0100

    home page responsivity

--- aside.php
+++ aside.php
@@ -27,13 +27,13 @@
         </div>
         <h2 class="asideSection__title">Browse Categories</h2>
         <div class="asideSection__categories">
-            <div class="asideSection__categories__categorie"> <h3 class="asideSection__categories__categorie__title">Sport</h3><img src="https://images.pexels.com/photos/841130/pexels-photo-841130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""></div>
+            <div class="asideSection__categories__categorie"><a href="blog.php?categorie=food"> <h3 class="asideSection__categories__categorie__title">Food</h3><img src="https://images.pexels.com/photos/1199957/pexels-photo-1199957.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""></a></div>
             
-            <div class="asideSection__categories__categorie"><h3 class="asideSection__categories__categorie__title">Lifestyle</h3><img src="https://images.pexels.com/photos/1153369/pexels-photo-1153369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""></div>
+            <div class="asideSection__categories__categorie"><a href="blog.php?categorie=lifestyle"><h3 class="asideSection__categories__categorie__title">Lifestyle</h3><img src="https://images.pexels.com/photos/1153369/pexels-photo-1153369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""></a></div>
             
-            <div class="asideSection__categories__categorie"><h3 class="asideSection__categories__categorie__title">Health</h3><img src="https://images.pexels.com/photos/40751/running-runner-long-distance-fitness-40751.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""></div>
+            <div class="asideSection__categories__categorie"><a href="blog.php?categorie=health"><h3 class="asideSection__categories__categorie__title">Health</h3><img src="https://images.pexels.com/photos/40751/running-runner-long-distance-fitness-40751.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""></a></div>
            
-            <div class="asideSection__categories__categorie"><h3 class="asideSection__categories__categorie__title">Science</h3><img src="https://images.pexels.com/photos/256262/pexels-photo-256262.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""></div>
+            <div class="asideSection__categories__categorie"><a href="blog.php?categorie=science"><h3 class="asideSection__categories__categorie__title">Science</h3><img src="https://images.pexels.com/photos/256262/pexels-photo-256262.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""></a></div>
            
         </div>
     </aside>
