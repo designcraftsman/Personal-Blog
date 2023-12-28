530576d84e8119452b8fb523e69af12746225c3b
Oussama Fayz <fzoussfr@gmail.com>
2023-12-28 15:13:41 +0100

    fixed the aside section responsivity issues

--- aside.php
+++ aside.php
@@ -27,9 +27,41 @@
         </div>
         <h2 class="asideSection__title">Browse Categories</h2>
         <div class="asideSection__categories">
-            <div class="asideSection__categories__categorie"><a href="blog.php?categorie=food"> <h3 class="asideSection__categories__categorie__title">Food</h3><img src="https://images.pexels.com/photos/1199957/pexels-photo-1199957.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""></a></div>
-            <div class="asideSection__categories__categorie"><a href="blog.php?categorie=lifestyle"><h3 class="asideSection__categories__categorie__title">Lifestyle</h3><img src="https://images.pexels.com/photos/1153369/pexels-photo-1153369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""></a></div>     
-            <div class="asideSection__categories__categorie"><a href="blog.php?categorie=health"><h3 class="asideSection__categories__categorie__title">Health</h3><img src="https://images.pexels.com/photos/40751/running-runner-long-distance-fitness-40751.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""></a></div>    
-            <div class="asideSection__categories__categorie"><a href="blog.php?categorie=science"><h3 class="asideSection__categories__categorie__title">Science</h3><img src="https://images.pexels.com/photos/256262/pexels-photo-256262.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""></a></div>
+            <div class="asideSection__categories__categorie">
+                <a href="blog.php?categorie=food">
+                    <div class="asideSection__categories__categorie__title">
+                        <h3 class="asideSection__categories__categorie__title__name">Food</h3>
+                        <p class="asideSection__categories__categorie__title__number">44</p>
+                    </div>
+                    <img src="https://images.pexels.com/photos/1199957/pexels-photo-1199957.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                </a>
+            </div>
+            <div class="asideSection__categories__categorie">
+                <a href="blog.php?categorie=lifestyle">
+                    <div class="asideSection__categories__categorie__title">
+                        <h3 class="asideSection__categories__categorie__title__name">Lifestyle</h3>
+                        <p class="asideSection__categories__categorie__title__number">54</p>
+                    </div>
+                    <img src="https://images.pexels.com/photos/1153369/pexels-photo-1153369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                </a>
+            </div>     
+            <div class="asideSection__categories__categorie">
+                <a href="blog.php?categorie=health">
+                    <div class="asideSection__categories__categorie__title">
+                        <h3 class="asideSection__categories__categorie__title__name">Health</h3>
+                        <p class="asideSection__categories__categorie__title__number">33</p>
+                    </div>
+                    <img src="https://images.pexels.com/photos/40751/running-runner-long-distance-fitness-40751.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                </a>
+            </div>    
+            <div class="asideSection__categories__categorie">
+                <a href="blog.php?categorie=science">
+                    <div class="asideSection__categories__categorie__title">
+                        <h3 class="asideSection__categories__categorie__title__name">Science</h3>
+                        <p class="asideSection__categories__categorie__title__number">17</p>
+                    </div>
+                    <img src="https://images.pexels.com/photos/256262/pexels-photo-256262.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+                </a>
+            </div>
         </div>
     </aside>
